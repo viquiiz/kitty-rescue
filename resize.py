b7153835ba7b85232bf5b51cebc3aed3a07245d6
Victória Zanella <vzanella@outlook.com.br>
2025-08-18 22:12:08 -0300

    fix apagando arquivos desnecessarios

--- resize.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-for x in os.listdir('assets\img\main_char'):
-    image = Image.open(f'assets\img\main_char\{x}')
-    print(f"Original size : {image.size}") # 5464x3640
-
-    sunset_resized = image.resize((128, 128))
-    sunset_resized.save(f'assets\img\main_char\{x}')
